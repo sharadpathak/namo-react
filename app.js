d9b7147f77bfc459a752465591d6dbb03c4ede23
sharadpathak <sharadpathak20@gmail.com>
2023-06-27 23:35:21 +0530

    Episode 01

---
+++ app.js
@@ -0,0 +1,21 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement(
+      "h1",
+      { id: "heading", style: { color: "red" } },
+      "Hello World ! Shaad"
+    ),React.createElement(
+        "h2",
+        { id: "heading", style: { color: "red" } },
+        "Hello World ! Shaad"
+      )]
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
