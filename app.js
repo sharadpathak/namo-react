62fd76e941c14707db5dc3574076ee7f913bee9d
Sharad Pathak <spathak1.it@hbplat.com>
2023-07-06 22:33:26 +0530

    laying the Foundation

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement(
-      "h1",
-      { id: "heading", style: { color: "red" } },
-      "Hello World ! Shaad"
-    ),React.createElement(
-        "h2",
-        { id: "heading", style: { color: "red" } },
-        "Hello World ! Shaad"
-      )]
-  )
-);
-
+const Title = () => {
+  return <h1 className="heading">Namo React</h1>;
+};
+const HeadingComponent = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <Title />
+        {Title()}
+        <p>lorem ipsum </p>
+      </div>
+    </React.Fragment>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent />);
